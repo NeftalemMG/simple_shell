80c3d393cf060333934b0c1346f3f74018019c5f
DENNYYESSAR <dennisokeri5@gmail.com>
2023-12-13 16:39:43 -0800

    ssFunctions1.c

--- ssFunctions1.c
+++ ssFunctions1.c
@@ -12,16 +12,16 @@
   */
 char *copyString(char *destination, char *source)
 {
-    int index;
-    int length; 
-	
+	int index;
+	int length;
+
 	length = stringLength(source);
 
-    for (index = 0; index <= length; index++)
+	for (index = 0; index <= length; index++)
 	{
 		destination[index] = source[index];
 	}
-    return (destination);
+	return (destination);
 }
 
 /**
@@ -34,28 +34,30 @@ char *copyString(char *destination, char *source)
  */
 int stringLength(char *str)
 {
-    int length;
-	
+	int length;
+
 	length = 0;
 
-    while (str[length] != '\0')
+	while (str[length] != '\0')
 	{
 		length++;
 	}
-    return (length);
+	return (length);
 }
 
-
 /**
- * stringCompare - compares two strings to find out if they are exactly the same
+ * stringCompare - compares two strings
+ * to find out if they are exactly the same
  * @userInput: user input supplied to search for
  * @compareString: string to compare against
  * @inputLength: length of userInput
  *
- * Description: This function compares two strings and returns 1 if they are equal,
+ * Description: This function compares
+ * two strings and returns 1 if they are equal,
  * -1 if they are not, and 0 if their lengths are different.
  *
- * Return: Returns 1 if the strings are identical, -1 if they differ, and 0 if their lengths are not the same
+ * Return: Returns 1 if the strings are identical,
+ * -1 if they differ, and 0 if their lengths are not the same
  */
 int stringCompare(char *userInput, char *compareString, unsigned int inputLength)
 {
@@ -68,6 +70,7 @@ int stringCompare(char *userInput, char *compareString, unsigned int inputLength
 		return (-1);
 	}
 	i = 0;
+
 	while (userInput[i] != '\0' && compareString[i] != '\0')
 	{
 		if (userInput[i] != compareString[i])
@@ -91,18 +94,18 @@ int stringCompare(char *userInput, char *compareString, unsigned int inputLength
   */
 int stringCompareN(char *str1, char *str2, unsigned int length)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < length)
-    {
-        if (str1[i] != str2[i])
+	i = 0;
+	while (i < length)
+	{
+	if (str1[i] != str2[i])
 		{
 			return (-1);
-		} 
-        i++;
-    }
-    return (1);
+		}
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -110,7 +113,6 @@ int stringCompareN(char *str1, char *str2, unsigned int length)
  *
  * Description: This function is responsible for terminating the shell program
  * and returning an exit status of -1.
- * 
  * Return: Returns -1 to indicate shell exit.
  *
  */
